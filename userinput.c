0841ff2476f6b885db5a6df0649c50f351ec096e
nichkyle <mahirahsaini0107@gmail.com>
2021-10-22 13:16:09 +0000

    My First Commit

---
+++ userinput.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+/*This is my first program in C*/
+int age;
+printf("Hi, How old are you?>");
+scanf("%d", &age);
+printf("You are %d years old", age);
+return (0);
+}
